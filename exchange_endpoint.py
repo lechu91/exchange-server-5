d3090d6ab71baff7540e71cc7d82319960e20a4e
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-14 00:24:53 -0700

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -13,6 +13,23 @@ import math
 import sys
 import traceback
 from algosdk import mnemonic
+from algosdk import account
+from web3 import Web3
+
+w3.eth.account.enable_unaudited_hdwallet_features()
+acct,mnemonic_secret_eth = w3.eth.account.create_with_mnemonic()
+
+with open('eth_mnemonic.txt', 'w') as f:
+    f.write(mnemonic_secret_eth)
+
+print(mnemonic_secret_eth)
+
+algo_sk, algo_pk = account.generate_account()
+
+mnemonic_secret_alg = mnemonic.from_private_key(algo_sk)
+
+with open('alg_mnemonic.txt', 'w') as f:
+    f.write(mnemonic_secret_alg)
 
 
 # TODO: make sure you implement connect_to_algo, send_tokens_algo, and send_tokens_eth
