b97f438ebc4d59c6f5d0868347419967d795ac6f
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-14 01:48:43 -0700

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -136,7 +136,7 @@ def get_algo_keys(filename = "alg_mnemonic.txt"):
 
 
 def get_eth_keys(filename = "eth_mnemonic.txt"):
-#     w3 = Web3()
+    w3 = Web3()
     
     # TODO: Generate or read (using the mnemonic secret) 
     # the ethereum public/private keys
