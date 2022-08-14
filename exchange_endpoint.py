5d0ecd613f6a700b8d0eb2e2f5496971dfaeba21
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-14 04:08:44 -0700

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -20,8 +20,9 @@ from web3 import Web3
 # Initialize variables
 w3 = Web3()
 w3.eth.account.enable_unaudited_hdwallet_features()
-fields_basic = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount','tx_id']
+
 fields_child = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount','creator_id','tx_id']
+new_order_fields = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount','tx_id','signature']
 
 # Generate Mnemonics
 
