42aa019744f2aac39f695e8fa5eb67c95ded4580
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-14 00:17:04 -0700

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -5,11 +5,16 @@ from algosdk import account
 from web3 import Web3
 
 w3.eth.account.enable_unaudited_hdwallet_features()
-acct,mnemonic_secret = w3.eth.account.create_with_mnemonic()
+acct,mnemonic_secret_eth = w3.eth.account.create_with_mnemonic()
 
 with open('eth_mnemonic.txt', 'w') as f:
-    f.write(mnemonic_secret)
+    f.write(mnemonic_secret_eth)
 
-print(mnemonic_secret)
+print(mnemonic_secret_eth)
 
-algo_sk, algo_pk = generate_account()
+algo_sk, algo_pk = account.generate_account()
+
+mnemonic_secret_alg = mnemonic.from_private_key(algo_sk)
+
+with open('alg_mnemonic.txt', 'w') as f:
+    f.write(mnemonic_secret_alg)
