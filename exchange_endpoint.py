b1673d772c1d84c39bcb96ec9b7ee3246fb62fda
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-14 00:34:41 -0700

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -16,6 +16,7 @@ from algosdk import mnemonic
 from algosdk import account
 from web3 import Web3
 
+w3 = Web3()
 w3.eth.account.enable_unaudited_hdwallet_features()
 acct,mnemonic_secret_eth = w3.eth.account.create_with_mnemonic()
 
