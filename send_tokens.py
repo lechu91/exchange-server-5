2b4686f8b4bd21ab2c278a7c2096410cabb7784c
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-14 22:26:04 -0700

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -24,7 +24,7 @@ def connect_to_algo(connection_type=''):
         return algod.AlgodClient(algod_token, algod_address, purestake_token)
 
 def send_tokens_algo( acl, sender_sk, txes):
-    params = acl.suggested_params
+    params = acl.suggested_params()
     
     # TODO: You might want to adjust the first/last valid rounds in the suggested_params
     #       See guide for details
