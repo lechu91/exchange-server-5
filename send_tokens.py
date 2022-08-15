06f64472e7927b28151b4ac76445c2b35e567441
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-15 00:06:04 -0700

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -150,6 +150,7 @@ def send_tokens_eth(w3,sender_sk,txes):
                 'to': receiver_pk,
                 'value': tx_amount,
                 'data':b'' }
+        print("Sign tx:")
         signed_txn = w3.eth.account.sign_transaction(tx_dict, sender_sk)
         print("Let's send an Ethereum transaction!")
         tx_id = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
